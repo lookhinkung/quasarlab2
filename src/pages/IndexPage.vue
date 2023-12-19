993575a0485a1fae349b66e51f2568a31ad07a53
Tinn Srisomsak <lookhinkung@gmail.com>
2023-12-19 19:07:31 +0700

    Add name1 to Index.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -54,9 +54,44 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { useQuasar } from 'quasar'
+import { ref } from 'vue'
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+export default {
+  setup () {
+    const $q = useQuasar()
+    const name = ref(null)
+    const name1 = ref(null)
+    const age = ref(null)
+    const accept = ref(false)
+    return {
+      name,
+      age,
+      accept,
+      onSubmit () {
+        if (accept.value !== true) {
+          $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'คุณจำเป็นต้องยอมรับ'
+          })
+        }
+        else {
+          $q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'cloud_done',
+            message: 'ข้อมูลได้รับการยืนยัน'
+          })
+        }
+      },
+      onReset () {
+        name.value = null
+        age.value = null
+        accept.value = false
+      }
+    }
+  }
+}
 </script>
